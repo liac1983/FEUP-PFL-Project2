1fe03a0226b15944f590bfa1fa680afcdb799eb2
Lara Cunha <up202108876@up.pt>
2023-12-06 12:33:21 +0000

    Part 1 em processo

--- TP2-quickstart.hs
+++ TP2-quickstart.hs
@@ -1,5 +1,7 @@
 -- PFL 2023/24 - Haskell practical assignment quickstart
 
+import Data.List (intercalate)
+
 -- Part 1
 
 -- Do not modify our definition of Inst and Code
@@ -18,16 +20,57 @@ createEmptyStack :: Stack
 createEmptyStack = []
 
 -- stack2Str :: Stack -> String
-stack2Str = undefined -- TODO, Uncomment all the other function type declarations as you implement them
+stack2Str :: Stack -> String
+stack2Str stack = unwords $ map show stack
+
+-- Add the following definition for State
+type State = [(String, Integer)]
 
 -- createEmptyState :: State
-createEmptyState = undefined -- TODO, Uncomment the function signature after defining State
+createEmptyState :: State
+createEmptyState = []
 
 -- state2Str :: State -> String
-state2Str = undefined -- TODO
+state2Str :: State -> String
+state2Str state = intercalate "," [var ++ "=" ++ show val | (var, val) <- state]
 
 -- run :: (Code, Stack, State) -> (Code, Stack, State)
-run = undefined -- TODO
+-- run :: (Code, Stack, State) -> (Code, Stack, State)
+run :: (Code, Stack, State) -> (Code, Stack, State)
+run ([], stack, state) = ([], stack, state)  -- If the code is empty, return the same state
+run (inst:code, stack, state) = case inst of
+  -- Implement the logic for each instruction
+  Push n -> run (code, n:stack, state)
+  Add -> run (code, performBinaryOperation (+) stack, state)
+  Mult -> run (code, performBinaryOperation (*) stack, state)
+  Sub -> run (code, performBinaryOperation (\x y -> y - x) stack, state)  -- Fix: Correct order of subtraction
+  Tru -> run (code, 1:stack, state)
+  Fals -> run (code, 0:stack, state)
+  Equ -> run (code, performBinaryOperation (\x y -> if x == y then 1 else 0) stack, state)
+  Le -> run (code, performBinaryOperation (\x y -> if x <= y then 1 else 0) stack, state)
+  And -> run (code, performBinaryOperation (\x y -> if x /= 0 && y /= 0 then 1 else 0) stack, state)
+  Neg -> run (code, negate (head stack):tail stack, state)
+  Fetch x -> run (code, lookupVar x state:stack, state)
+  Store x -> run (code, tail stack, updateVar x (stack !! 0) state)  -- Fix: Use value from the stack
+  Noop -> run (code, stack, state)
+  Branch c1 c2 -> if head stack /= 0 then run (c1 ++ code, tail stack, state)
+                  else run (c2 ++ code, tail stack, state)
+  Loop c1 c2 -> if head stack /= 0 then run (c1 ++ [Branch (c2 ++ [Loop c1 c2]) [Noop]] ++ code, tail stack, state)
+                else run (code, tail stack, state)
+
+-- Helper function to perform binary operations on the stack
+performBinaryOperation :: (Integer -> Integer -> Integer) -> Stack -> Stack
+performBinaryOperation op (x:y:rest) = op y x : rest
+
+-- Helper function to lookup variable in the state
+lookupVar :: String -> State -> Integer
+lookupVar x state = case lookup x state of
+  Just val -> val
+  Nothing -> error ("Variable not found: " ++ x)
+
+-- Helper function to update variable in the state
+updateVar :: String -> Integer -> State -> State
+updateVar x val state = filter (\(var, _) -> var /= x) state ++ [(x, val)]
 
 -- To help you test your assembler
 testAssembler :: Code -> (String, String)
@@ -35,7 +78,7 @@ testAssembler code = (stack2Str stack, state2Str state)
   where (_,stack,state) = run(code, createEmptyStack, createEmptyState)
 
 -- Examples:
--- testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
+-- testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","") Passou 
 -- testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
 -- testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
 -- testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
